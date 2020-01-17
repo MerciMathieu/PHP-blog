8f28e4e343759415bd5bfcfafda440edab0c4d54
MerciMathieu <mathieu.delclos@laposte.net>
2020-01-17 18:14:05 +0100

    restructured controllers folder

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Controller\HomeController;
-use App\Controller\ContactController;
+use App\Controller\Home\HomeController;
+use App\Controller\Contact\ContactController;
 
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader, [
