4321157c68518af8201cb5514e40165f6eddf71d
MerciMathieu <mathieu.delclos@laposte.net>
2020-05-05 13:42:16 +0200

    Contact form validation input

--- src/Controller/Home/HomeController.php
+++ src/Controller/Home/HomeController.php
@@ -8,13 +8,18 @@ class HomeController extends AbstractController
 {
     public function index()
     {
+        $post = null;
+
         if (isset($_POST['submit'])) {
+            $post = $_POST;
             $this->formValidation();
         }
 
-        return $this->twig->render('homepage/homepage.html.twig');
+        return $this->twig->render('homepage/homepage.html.twig', [
+            'postVariables' => $post
+        ]);
     }
-  
+
     private function sendMail(): void
     {
         $to      = 'mathieu.delclos@gmail.com';
@@ -24,17 +29,40 @@ class HomeController extends AbstractController
         'Reply-To: ' . $_POST['email'] . "\r\n" ;
 
         mail($to, $subject, $message, $headers);
+
+        header('Location: /');
     }
 
     private function formValidation(): void
     {
-        if (isset($_POST['firstname']) and !empty($_POST['firstname']) and
-            isset($_POST['lastname']) and !empty($_POST['lastname']) and
-            isset($_POST['email']) and !empty($_POST['email']) and
-            isset($_POST['message']) and !empty($_POST['message'])) {
-                $this->sendMail();
-        } else {
-            #TODO RENVOYER UNE ERREUR si la validation n'est pas ok
+        $success = true;
+        $errors = [];
+
+        if (!isset($_POST['firstname']) or empty($_POST['firstname']) or strlen($_POST['firstname']) < 3 ) {
+            $success = false;
+            $errors['firstname'] = "Le prénom doit contenir au moins 3 caractères";
         }
+
+        if (!isset($_POST['lastname']) or empty($_POST['lastname']) or strlen($_POST['lastname']) < 3 ) {
+            $success = false;
+            $errors['lastname'] = "Le nom doit contenir au moins 3 caractères";
+        }
+
+        if (!isset($_POST['email']) or empty($_POST['email']) or !preg_match ( " /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/ " , $_POST['email'])) {
+            $success = false;
+            $errors['email'] = "L'email doit être valide";
+        }
+
+        if (!isset($_POST['message']) or empty($_POST['message']) or strlen($_POST['message']) < 3 ) {
+            $success = false;
+            $errors['message'] = "Le message doit contenir au moins 3 caractères";
+        } 
+
+        var_dump($errors);
+        var_dump($success);
+
+        if ($success === true) {
+            $this->sendMail();
+        }      
     }
 }
